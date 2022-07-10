74a7eac019a6f0caad452ab1cb9f4c249461bdcb
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-10 12:30:37 +0200

    Test some RestAction subtypes

--- JDAction-Maven-Test/src/main/java/Test.java
+++ JDAction-Maven-Test/src/main/java/Test.java
@@ -1,5 +1,6 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
@@ -16,6 +17,16 @@ public class Test {
 				.awaitReady();
 
 		jda.retrieveUserById(0);
+	}
+
+	public void testTypes() {
+		Guild g = null;
+
+		g.retrieveAuditLogs();
+
+		g.updateCommands();
+
+		g.modifyRolePositions();
 	}
 
 	public class Inner {
